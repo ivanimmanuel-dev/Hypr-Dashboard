906c941439ef02ac2c4e049af164826636a2345e
Ivan Immanuel <ivanimmanuel1234@gmail.com>
2026-04-09 20:32:16 -0400

    Fixed install.sh: removed konsole, added glava yay check

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 set -e
+#-----------------------------------------------------------------------------------------------------------------------------------------------
+#                                                                   INSTALLER SCRIPT
+#-----------------------------------------------------------------------------------------------------------------------------------------------
 
 # Installs the necessary dependencies for the dashboard
 echo "Installing dependencies..."
-sudo pacman -Syu --needed kitty fastfetch cmatrix btop konsole
-yay -S glava
 
-# Checks if dashboard script exits
+sudo pacman -Syu --needed kitty fastfetch tty-clock btop
+
+if command -v yay >/dev/null 2>&1; then
+    yay -S glava
+else
+    echo "yay not found. Please install glava manually: https://github.com/jarcode-foss/glava"
+fi
+
+# Checks if dashboard script exists
 if [[ ! -f dashboard.sh ]]; then
     echo "dashboard.sh not found in this folder. Please run this from the folder containing the script."
     exit 1
